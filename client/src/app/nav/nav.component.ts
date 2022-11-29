ba6e8e37a90ab3e26683da6a34fd42c230f83b7e
Vikram Jadhav <vikramjadhav1015@gmail.com>
2022-11-29 08:34:04 +0530

    Persisting the login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {};
+  loggedIn = false;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe({
+      next: (user) => this.loggedIn = !!user,
+      error: (error) => console.log(error)
+    });
   }
 
   login(){
+    this.accountService.login(this.model).subscribe({
+      next:(Response) => {
+        console.log(Response);
+        this.loggedIn =true;
+      },
+      error : (error) =>{
+        console.log(error);
+      }
+    });
     console.log(this.model);
+  }
+
+  logout(){
+    this.accountService.logout();
+    this.loggedIn = false;
   }
 }
